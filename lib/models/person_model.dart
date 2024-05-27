3c14dfe2617143f5e34a45ddb7dce1db04719a6a
hammadx02 <hammadaali02@gmail.com>
2024-05-27 23:27:14 +0500

    changes

--- lib/models/person_model.dart
+++ lib/models/person_model.dart
@@ -3,5 +3,5 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 @freezed
 class PersonModel with _$PersonModel {
 
-  PersonModel({String? name}) = _PersonModel
+  PersonModel({String? name}) = _PersonModel;
 }
